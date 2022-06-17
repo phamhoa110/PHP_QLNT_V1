037c2653f4865b37086a5aa5b51d0e389b3d9365
phamhoa110 <phamhoa1102001@gmail.com>
2022-06-17 11:31:21 +0700

    sua logo nhung ma chua duoc

--- admin/index.php
+++ admin/index.php
@@ -43,7 +43,7 @@ if(!isset($_SESSION['dangnhap1'])){
 
   <!-- Preloader-->
   <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="dist/img/logo.png" alt="Logo" height="60" width="60">
+    <img class="animation_shake" src="dist/img/logo-noi-that-2.png" alt="Logo" height="60" width="60">
   </div>
 
   <?php 
